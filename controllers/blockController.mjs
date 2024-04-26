0555833d6df49575ce23a11a15cb4b3cfec8022d
Rowel Malmström <143719030+rowpacmal@users.noreply.github.com>
2024-04-26 17:09:57 +0200

    create func to get 1 block by index/number

--- controllers/blockController.mjs
+++ controllers/blockController.mjs
@@ -1,26 +1,34 @@
 import Block from '../models/Block.mjs';
-import { createRequire } from 'module';
 import FileHandler from '../utils/FileHandler.mjs';
+
+import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 const blocks = require('../data/blocks.json');
 
-const folderName = 'data';
-const fileName = 'blocks.json';
-
 export const getAllBlocks = (req, res, next) => {
-  res.json({ data: blocks });
+  res.json({ items: blocks.length, data: blocks });
+};
+
+export const getBlockByIndex = (req, res, next) => {
+  const block = blocks.find((b) => b.index === +req.params.index);
+
+  if (!block) {
+    return next();
+  }
+
+  res.json({ data: block });
 };
 
 export const createNewBlock = (req, res, next) => {
   const block = new Block(
     blocks.length + 1,
-    { type: 'data' },
-    blocks[blocks.length - 1].hash
+    { ...req.body },
+    blocks.length > 0 ? blocks[blocks.length - 1].hash : null
   );
 
   blocks.push(block);
 
-  new FileHandler(folderName, fileName).write(blocks);
+  new FileHandler('data', 'blocks.json').write(blocks);
 
   res.json({ data: block });
 };
--- routes/blockRoutes.mjs
+++ routes/blockRoutes.mjs
@@ -1,11 +1,14 @@
 import express from 'express';
 import {
   getAllBlocks,
+  getBlockByIndex,
   createNewBlock,
 } from '../controllers/blockController.mjs';
 
 const router = express.Router();
 
 router.route('/').get(getAllBlocks).post(createNewBlock);
+
+router.route('/:index').get(getBlockByIndex);
 
 export default router;

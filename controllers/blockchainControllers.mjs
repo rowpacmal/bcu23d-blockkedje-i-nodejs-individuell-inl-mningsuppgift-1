d5d4c27cfd0effe7a470667b8cc5660337e39320
Rowel Malmström <143719030+rowpacmal@users.noreply.github.com>
2024-05-04 11:16:49 +0200

    Creat Block.mjs & Blockchian.mjs; Update blockchainControllers.mjs

--- controllers/blockchainControllers.mjs
+++ controllers/blockchainControllers.mjs
@@ -1,17 +1,37 @@
+import { blockchain } from '../startup.mjs';
+
 const getBlockchain = (req, res, next) => {
-  res.json({ msg: 'get blockchain' });
+  res.json({ data: blockchain });
 };
 
 const getLatestBlock = (req, res, next) => {
-  res.json({ msg: 'get the latest' });
+  res.json({ data: blockchain.getLastBlock() });
 };
 
 const getBlockByIndex = (req, res, next) => {
-  res.json({ msg: `get block by index ${req.params.index}` });
+  const index = +req.params.index;
+  const block = blockchain.chain[index];
+
+  if (!block) {
+    res.json({ error: `Cannot find block with index ${index}` });
+    return;
+  }
+
+  res.json({ data: block });
 };
 
 const mineBlock = (req, res, next) => {
-  res.json({ msg: 'mine new block' });
+  const block = blockchain.proofOfWork(req.body);
+
+  res.json({ data: block });
 };
 
-export { getBlockchain, getLatestBlock, getBlockByIndex, mineBlock };
+const synchronizeChain = (req, rws, next) => {};
+
+export {
+  getBlockchain,
+  getLatestBlock,
+  getBlockByIndex,
+  mineBlock,
+  synchronizeChain,
+};

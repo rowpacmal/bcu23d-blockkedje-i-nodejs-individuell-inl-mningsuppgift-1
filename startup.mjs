75a67230e8958022062737935721bcee7825bf84
Rowel Malmström <143719030+rowpacmal@users.noreply.github.com>
2024-05-04 15:43:04 +0200

    Update server.mjs & startup.mjs

--- startup.mjs
+++ startup.mjs
@@ -1,10 +1,31 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 import Blockchain from './models/Blockchain.mjs';
+import FileHandler from './utils/FileHandler.mjs';
+
+global.__appdir = dirname(fileURLToPath(import.meta.url));
 
 const setupBlockchain = () => {
-  let blockchain = new Blockchain();
+  const blockchainJSON = new FileHandler(
+    'data',
+    `blockchain-${process.argv[2]}.json`
+  );
+
+  let blockchain = blockchainJSON.read(true);
+
+  if (Object.keys(blockchain).length === 0) {
+    blockchain = new Blockchain();
+
+    blockchainJSON.write(blockchain);
+  } else {
+    Object.setPrototypeOf(blockchain, Blockchain.prototype);
+  }
+
   return blockchain;
 };
 
 const blockchain = setupBlockchain();
+const PORT = process.argv[2] || process.env.PORT || 5000;
 
-export { blockchain };
+export { blockchain, PORT };
